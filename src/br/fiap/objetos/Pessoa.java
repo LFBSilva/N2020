7b0a2d655a7f1745288767009ab3e2edda14620f
LFBSilva <lfbsilva.93@gmail.com>
2020-06-16 21:34:50 -0300

    Adiciação de peso e altura na classe Pessoa.

--- src/br/fiap/objetos/Pessoa.java
+++ src/br/fiap/objetos/Pessoa.java
@@ -5,34 +5,63 @@ public abstract class Pessoa {
 	protected int idade;
 	protected String sexo;
 	protected String cpf;
-	public Pessoa(String nome, int idade, String cpf) {
+	protected float altura;
+	protected float peso;
+
+	public Pessoa(String nome, int idade, String cpf, float altura, float peso) {
 		this.nome = nome;
 		this.idade = idade;
 		this.cpf = cpf;
+		this.altura = altura;
+		this.peso = peso;
 	}
+
+	public float getAltura() {
+		return altura;
+	}
+
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public int getIdade() {
 		return idade;
 	}
+
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
+
 	public String getSexo() {
 		return sexo;
 	}
+
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-	
- }
+
+}
